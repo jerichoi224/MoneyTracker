ef343a16d24ce957e367d6a1f5bd36e80b818caf
jerichoi224 <jerichoi224@gmail.com>
2020-07-30 01:42:55 -0400

    Added better splash Screens

--- lib/SplashWidget.dart
+++ lib/SplashWidget.dart
@@ -4,7 +4,8 @@ import "package:intl/intl.dart";
 
 class SplashWidget extends StatefulWidget {
   final Map<String, double> data;
-  final myController = TextEditingController();
+  final dateController = TextEditingController();
+  final amountController = TextEditingController();
   SplashWidget({Key key, this.data}) : super(key: key);
 
   @override
@@ -12,8 +13,10 @@ class SplashWidget extends StatefulWidget {
 }
 
 class _SplashState extends State<SplashWidget>{
-  @override
+  final pageController = PageController(initialPage: 0);
+  int _currentIndex = 0;
 
+  @override
   void initState(){
     super.initState();
 
@@ -29,6 +32,13 @@ class _SplashState extends State<SplashWidget>{
     return double.tryParse(s) != null;
   }
 
+  bool isDate(String s) {
+    if(s == null) {
+      return false;
+    }
+    return int.tryParse(s) != null && int.parse(s) < 32 && int.parse(s) > 0;
+  }
+
   finishSplash() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool('seen', true);
@@ -36,98 +46,286 @@ class _SplashState extends State<SplashWidget>{
     Navigator.of(context).pushNamedAndRemoveUntil('/home', (Route<dynamic> route) => false);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body:
-        Builder(
-          builder: (context) =>
-            new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(15),
-                  child: Center(
-                      child: Image(
-                        image: AssetImage('assets/my_icon.png'),
-                        width: 150,
-                      )
+  nextPage() {
+    setState(() {
+      _currentIndex += 1;
+      pageController.animateToPage(_currentIndex, duration: Duration(milliseconds: 500), curve: Curves.ease);
+    });
+  }
+
+  showSnackBar(BuildContext context, String s){
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text(s),
+      duration: Duration(seconds: 3),
+    ));
+  }
+
+  Widget introScreen(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(15),
+          child: Center(
+              child: Image(
+                image: AssetImage('assets/my_icon.png'),
+                width: 150,
+              )
+          ),
+        ),
+        Padding(
+            padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+            child: Center(
+                child: Text(
+                  "Welcome to Money Tracker!",
+                  style: TextStyle(
+                    fontSize: 20,
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
-                  child: Center(
-                      child: Text(
-                        "How much would you like to \nspend daily on average?",
+                )
+            )
+        ),
+        Padding(
+            padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+            child: Center(
+                child: Text(
+                  "1. Choose your monthly reset date\n"
+                  "2. Set your Daily Limits\n"
+                  "3. Keep recording your spendings",
+                  style: TextStyle(
+                    fontSize: 17,
+                    height: 1.4
+                  ),
+                )
+            )
+        ),
+        Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+            margin: EdgeInsets.fromLTRB(40, 10, 40, 10),
+            color: Color.fromRGBO(149, 213, 178, 1),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  ListTile(
+                      onTap:(){
+                        nextPage();
+                      },
+                      title: Text("Next",
                         style: TextStyle(
-                          fontSize: 20,
+                          fontSize: 18,
                         ),
+                        textAlign: TextAlign.center,
                       )
                   )
+                ]
+            )
+        )
+      ],
+    );
+  }
+
+  Widget monthlyResetDate(BuildContext context) {
+    return Builder(
+      builder: (context) => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Center(
+                child: Icon(
+                  Icons.calendar_today,
+                  size: 150,
+                  color: Color.fromRGBO(149, 213, 178, 1),
                 ),
-                Card(
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-                  margin: EdgeInsets.fromLTRB(40, 0, 40, 0),
-                  child: Center(
-                    child: ListTile(
-                        title: new Row(
-                          children: <Widget>[
-                            Flexible(
-                                child: TextField(
-                                  controller: widget.myController,
-                                  decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: 'Enter Daily Limit',
-                                  ),
-                                  keyboardType: TextInputType.number,
-                                  textAlign: TextAlign.center,
-                                )
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Center(
+              child: Text(
+                 "One what day would you like\nthe monthly cycle to reset?",
+                 style: TextStyle(
+                   fontSize: 20,
+                 ),
+               )
+            ),
+         ),
+         Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 20),
+            child: Center(
+               child: Text(
+                 "(Try to do before the 28th, considering February)",
+                 style: TextStyle(
+                 fontSize: 15,
+                 color: Colors.black54
+                   ),
+                 )
+             )
+          ),
+          Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+              margin: EdgeInsets.fromLTRB(40, 0, 40, 0),
+              child: Center(
+                child: ListTile(
+                    title: new Row(
+                      children: <Widget>[
+                        Flexible(
+                            child: TextField(
+                              controller: widget.dateController,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Enter Date',
+                              ),
+                              keyboardType: TextInputType.number,
+                              textAlign: TextAlign.center,
                             )
-                          ],
                         )
-                    ),
-                  )
+                      ],
+                    )
                 ),
-                Card(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-                    margin: EdgeInsets.fromLTRB(40, 10, 40, 10),
-                    color: Color.fromRGBO(149, 213, 178, 1),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+              )
+          ),
+          Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+              margin: EdgeInsets.fromLTRB(40, 10, 40, 10),
+              color: Color.fromRGBO(149, 213, 178, 1),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    ListTile(
+                        onTap:(){
+                          if(widget.dateController.text.isNotEmpty) {
+                            if(isDate(widget.dateController.text)) {
+                              _saveSP("monthlyResetDate", double.parse(widget.dateController.text));
+                              nextPage();
+                            }else{
+                              showSnackBar(context, "Your input is invalid. Please Check again");
+                            }
+                          }else{
+                            showSnackBar(context, "Please enter a reset date");
+                          }
+                        },
+                        title: Text("Next",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                          textAlign: TextAlign.center,
+                        )
+                    )
+                  ]
+              )
+          )
+        ],
+      )
+    );
+  }
+
+  Widget dailyLimit(BuildContext context){
+    return Builder(
+        builder: (context) => Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: Center(
+                child: Icon(
+                  Icons.attach_money,
+                  size: 190,
+                  color: Color.fromRGBO(149, 213, 178, 1),
+                ),
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+                child: Center(
+                    child: Text(
+                      "How much would you like to \nspend daily on average?",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    )
+                )
+            ),
+            Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                margin: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                child: Center(
+                  child: ListTile(
+                      title: new Row(
                         children: <Widget>[
-                          ListTile(
-                              onTap:(){
-                                if(widget.myController.text.isNotEmpty) {
-                                  if(isNumeric(widget.myController.text)) {
-                                    _saveSP("dailyLimit", double.parse(widget.myController.text));
-                                    finishSplash();
-                                  }else{
-                                    Scaffold.of(context).showSnackBar(SnackBar(
-                                      content: Text('Your input is invalid. Please Check again'),
-                                      duration: Duration(seconds: 3),
-                                    ));
-                                  }
-                                }else{
-                                  Scaffold.of(context).showSnackBar(SnackBar(
-                                    content: Text('Please enter initial amount'),
-                                    duration: Duration(seconds: 3),
-                                  ));
-                                }
-                              },
-                              title: Text("Start",
-                                style: TextStyle(
-                                  fontSize: 18,
+                          Flexible(
+                              child: TextField(
+                                controller: widget.amountController,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: 'Enter Daily Limit',
                                 ),
+                                keyboardType: TextInputType.number,
                                 textAlign: TextAlign.center,
                               )
                           )
-                        ]
-                    )
+                        ],
+                      )
+                  ),
                 )
-              ],
+            ),
+            Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                margin: EdgeInsets.fromLTRB(40, 10, 40, 10),
+                color: Color.fromRGBO(149, 213, 178, 1),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      ListTile(
+                          onTap:(){
+                            if(widget.amountController.text.isNotEmpty) {
+                              if(isNumeric(widget.amountController.text)) {
+                                _saveSP("dailyLimit", double.parse(widget.amountController.text));
+                                finishSplash();
+                              }else{
+                                showSnackBar(context, "Your input is invalid. Please Check again");
+                              }
+                            }else{
+                              showSnackBar(context, "Please enter initial amount");
+                            }
+                          },
+                          title: Text("Start",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                            textAlign: TextAlign.center,
+                          )
+                      )
+                    ]
+                )
             )
+          ],
         )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> introPages = <Widget>[
+      introScreen(context),
+      monthlyResetDate(context),
+      dailyLimit(context),
+    ];
+    return Scaffold(
+      body:
+        Builder(
+          builder: (context) => PageView(
+          physics:new NeverScrollableScrollPhysics(),
+          onPageChanged: (index) {
+            FocusScope.of(context).unfocus();
+            _currentIndex = index;
+          },
+           controller: pageController,
+            children: introPages
+          )
+      ),
     );
   }
 
